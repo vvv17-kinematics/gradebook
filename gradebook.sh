a85c67882da4ea1a454017a639a1bb7272c6dbd1
Ugo Pattacini <ugo.pattacini@iit.it>
2016-12-31 21:24:58 +0100

    wip

--- gradebook.sh
+++ gradebook.sh
@@ -9,9 +9,10 @@
 # - jq
 
 org=vvv17-kinematics
+curdir=$(pwd)
 
 rm build -rf
 mkdir build && cd build
 
 git clone https://github.com/vvv-school/vvv-school.github.io.git
-./vvv-school/github.io/scripts/gradebook.sh $org $(pwd)/../
+./vvv-school.github.io/scripts/gradebook.sh $org $curdir
