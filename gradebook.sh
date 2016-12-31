166ccaf500c1a8977d129913f821bbe34f9ecdf5
Ugo Pattacini <ugo.pattacini@iit.it>
2016-12-31 21:03:34 +0100

    started coding the script for grading

---
+++ gradebook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Copyright: (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
+# Authors: Ugo Pattacini <ugo.pattacini@iit.it>
+# CopyPolicy: Released under the terms of the GNU GPL v3.0.
+
+# Dependencies (through apt-get):
+# - curl
+# - jq
+
+org=vvv17-kinematics
+
+rm build -rf
+mkdir build && cd build
+
+git clone https://github.com/vvv-school/vvv-school.github.io.git
+./vvv-school/github.io/scripts/gradebook.sh $org $(pwd)/../
